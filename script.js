1c1d683b68f81ae67c99d2fe3a070d9b100502c9
theOutcast43 <kokehinc@gmail.com>
2023-08-09 13:25:44 +0100

    add GAME function to play the game 5 times

--- script.js
+++ script.js
@@ -4,15 +4,13 @@ function users(){
     const userChoice = prompt('rock, paper or scissors?');
     return userChoice.toLowerCase()
 }
-let user = users();
-console.log(user);
+const user = users();
 
 function computerThings() {
     const getComputerChoice = ['rock', 'paper', 'scissors'];
     return getComputerChoice[Math.floor(Math.random() * getComputerChoice.length)];
 }
-let comp = computerThings();
-console.log(comp);
+const comp = computerThings();
 
 function checkWinner(user,comp){
     if(user === comp) return `tie`;
@@ -36,4 +34,33 @@ function playground(user,comp){
         return `You Lose! ${comp} beats ${user}`;
     }
 }
-console.log(playground(user,comp))
+
+function game(){
+    console.log('Welcome')
+    let playerScore = 0;
+    let computerScore = 0;
+    for(i = 0; i < 5; i++){
+        const user = users();
+        const comp = computerThings();
+        console.log(playground(user,comp));
+        console.log('------');    //for spacing between rounds
+        if(checkWinner(user,comp) == `Player`){
+            playerScore++;
+        }
+        else if(checkWinner(user,comp) == `Computer`){
+            computerScore++;
+        }
+    }
+    console.log(`Game Over`)
+    if(playerScore > computerScore){
+        console.log(`You Win!`)
+    } 
+    else if(playerScore < computerScore){
+        console.log(`Computer Wins!`)
+    }
+    else {
+        console.log(`We Have a Tie`)
+    }
+}
+
+game()
